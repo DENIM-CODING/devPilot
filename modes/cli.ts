8684ceb4e0fb67c807367dc4cba356e64062be31
DENIM <khannadenim@gmail.com>
2026-09-13 16:16:47 +0530

    implement plan mode and integrate web tools and scraper

--- modes/cli.ts
+++ modes/cli.ts
@@ -2,6 +2,7 @@ import chalk from "chalk";
 import { select, isCancel } from "@clack/prompts";
 import { runAgentMode } from "./agent/orchestrator";
 import { runAskMode } from "./ask/orchestrator";
+import { runPlanMode } from "./plan/orchestrator";
 // import { runAgentMode } from "./agent/orchestrator";
 // import { runAskMode } from "./ask/orchestrator";
 // import { runPlanMode } from "./plan/orchestrator";
@@ -30,7 +31,7 @@ export async function runCliMode() {
     }
     if (mode === "plan") {
         console.log("plan")
-        //await runPlanMode()
+        await runPlanMode()
     }
 
     if (mode !== "agent" && mode !== "plan" && mode !== "ask") {
